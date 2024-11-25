82c7bdd736f20f34faa43f9fc59d46393db29271
nonanonno <as.nonanonno@gmail.com>
2024-11-25 22:00:59 +0900

    feat: change to console script

--- README.md
+++ README.md
@@ -1,31 +1,68 @@
 # waypoint modifier
 
-waypoint を編集するツール
+waypoint を編集及び可視化するツール
 
 ## 前提環境
 
 - python (>=3.10)
-- poetry ([インストール方法](https://python-poetry.org/docs/#installing-with-pipx))
+- pip (>= 22.0.2)
 
-## インストール
+## Getting Started
+
+### インストール
 
 ```shell
-poetry install
+pip install -e .
+
+# 必要に応じて
+export PATH=$PATH:$HOME/.local/bin
 ```
 
-## 点列の編集
+### 点列の編集
 
-各 jupyter notebook を実行することで点列を編集できる。notebook は最初のセルに入出力ファイル名、次のセルに実行パラメータが書かれているため、それを修正した上で実行する。
+```shell
+resampler <path/to/localization-pose.csv> [-o <path/to/output.csv>]
+```
 
-notebook はコピーして (`waypoint_modifier` がインストールされた環境であれば) 任意の場所に置くことができる。
+### 点列の描画
 
-### resample.ipynb
+```shell
+viewer <path/to/waypoint.csv>
+```
 
-resample では点列を、距離及び曲率を使って間引く。0番目の点から走査していき、以下のOR条件を満たした時にその点を採用する。
+以下のようなメッセージが出るので、URLをブラウザでアクセスする。
+
+```
+Dash is running on http://127.0.0.1:8050/
+
+ * Serving Flask app 'waypoint_modifier.viewer'
+ * Debug mode: on
+```
+
+
+## 詳細
+
+### 点列の編集
+
+入力の（例えば自己位置推定の）Poseリストを間引く。0番目の点から走査していき、以下のOR条件を満たした時にその点を採用する。
 
 - 点の間隔の累和が `MAX_ACCUM_DIST` を越えた時
 - 曲率の累和が `MAX_ACCUM_CURV` を越えた時
 
 ただし、 停止時に点が重複するのを除去するために `MIN_ACCUM_DIST` 以上の距離を走行していることを制約に加えている。
 
-![img](./docs/resample.png)
+出力時は座標の列名を `x` と `y` に変更する。また、入力のCSVに`action` 列がある場合、それをコピーする。ない場合空の`action`列を追加する。
+
+
+### 点列の描画
+
+waypoint の CSVを可視化するWebアプリ。pip インストール時にインターネットアクセスが必要だが、一度インストールすればオフラインで動く(はず)。
+以下の列が必要。
+
+- x : x座標
+- y : y座標
+- action : その点でのアクション (空白 or `continue` or `stop`)
+
+`i` → `i+1` の点のベクトルを使って yaw角を計算している。これは [follow_path.py](https://github.com/AbudoriLab-TC2024/penguin_nav?tab=readme-ov-file#follow_pathpy)と同様の実装となる。0番目の点のyaw角は0になる。
+
+`viewer` 自体に点列を編集機能はない。読み込んだファイルを監視しているので、ファイルを編集すればそれが反映される。
---
+++ src/waypoint_modifier/resample.py
@@ -0,0 +1,167 @@
+import numpy as np
+import pandas as pd
+import argparse
+import os
+
+
+class Resampler:
+    def __init__(
+        self,
+        x_col,
+        y_col,
+        curvature_col,
+        distance_col,
+        window,
+        min_r,
+        min_d,
+        max_d,
+        max_c,
+    ):
+        self._x_col = x_col
+        self._y_col = y_col
+        self._curvature_col = curvature_col
+        self._distance_col = distance_col
+        self._window = window
+        self._min_r = min_r
+        self._min_d = min_d
+        self._max_d = max_d
+        self._max_c = max_c
+
+    def set_curvature(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Compute the curvatures of a trajectory.
+        """
+        ret = []
+        for i in range(len(df)):
+            if i < self._window or i >= len(df) - self._window:
+                ret.append(0.0)
+            else:
+                x = df[self._x_col].values[i - self._window : i + self._window + 1]
+                y = df[self._y_col].values[i - self._window : i + self._window + 1]
+
+                A = np.vstack((x, y, np.ones(len(x)))).T
+                v = -(x**2 + y**2)
+                u, *_ = np.linalg.lstsq(A, v, rcond=None)
+
+                cx_pred = u[0] / -2
+                cy_pred = u[1] / -2
+                r_pred = np.sqrt(cx_pred**2 + cy_pred**2 - u[2])
+                r_pred = max(r_pred, self._min_r)
+                ret.append(1.0 / r_pred)
+
+        df[self._curvature_col] = pd.Series(ret, index=df.index)
+
+        return df
+
+    def set_distance(self, df: pd.DataFrame) -> pd.DataFrame:
+        df[self._distance_col] = np.sqrt(
+            df[self._x_col].diff() ** 2 + df[self._y_col].diff() ** 2
+        ).fillna(0)
+        return df
+
+    def do_sampling(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Sample waypoints based on distance.
+        """
+        dist = 0.0
+        curv = 0.0
+
+        ret = []
+        for i in range(len(df)):
+            dist += df[self._distance_col].values[i]
+            curv += df[self._curvature_col].values[i]
+
+            if (dist > self._max_d or curv > self._max_c) and dist > self._min_d:
+                ret.append(df.iloc[i])
+                dist = 0.0
+                curv = 0.0
+
+        ret_df = pd.DataFrame(ret).copy()
+        ret_df.reset_index(drop=True, inplace=True)
+        self.set_distance(ret_df)
+
+        return ret_df
+
+    def resample(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = self.set_curvature(df)
+        df = self.set_distance(df)
+        return self.do_sampling(df)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=str, help="Input CSV file")
+    parser.add_argument("--output", "-o", type=str, help="Output CSV file")
+    parser.add_argument(
+        "--window",
+        type=int,
+        default=20,
+        help="Curvature computation window size (default: 20)",
+    )
+    parser.add_argument(
+        "--min_radius",
+        type=float,
+        default=0.1,
+        help="Minimum radius of curvature computation [m] (default: 0.1)",
+    )
+    parser.add_argument(
+        "--min_accum_dist",
+        type=float,
+        default=0.2,
+        help="Minimum accumulation distance between points when resampling [m] (default: 0.2)",
+    )
+    parser.add_argument(
+        "--max_accum_dist",
+        type=float,
+        default=2,
+        help="Maximum accumulation distance between points when resampling [m] (default: 2)",
+    )
+    parser.add_argument(
+        "--max_accum_curv",
+        type=float,
+        default=3,
+        help="Maximum accumulation curvature between points when resampling [1/m] (default: 3)",
+    )
+    parser.add_argument(
+        "--simple-xy-col",
+        action="store_true",
+        help="Use 'x', 'y' for the input csv columns name instead of '/pcl_pose/pose/pose/positon/x,y'",
+    )
+
+    args = parser.parse_args()
+
+    x_col, y_col = (
+        ("x", "y")
+        if args.simple_xy_col
+        else ("/pcl_pose/pose/pose/position/x", "/pcl_pose/pose/pose/position/y")
+    )
+
+    src_df = pd.read_csv(args.input)
+    resampler = Resampler(
+        x_col=x_col,
+        y_col=y_col,
+        curvature_col="curvature",
+        distance_col="distance",
+        window=args.window,
+        min_r=args.min_radius,
+        min_d=args.min_accum_dist,
+        max_d=args.max_accum_dist,
+        max_c=args.max_accum_curv,
+    )
+
+    dst_df = resampler.resample(src_df)
+
+    # rename columns to simplify the output
+    dst_df = dst_df.rename(columns={x_col: "x", y_col: "y"})
+
+    dst_filename = (
+        args.output if args.output else os.path.splitext(args.input)[0] + "_sparse.csv"
+    )
+    if "action" not in dst_df.columns:
+        dst_df["action"] = ""
+
+    dst_df[["x", "y", "action"]].to_csv(dst_filename)
+
+
+if __name__ == "__main__":
+    main()
